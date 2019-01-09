42b84bb46556d59bd8317c49339f94bb7ffed69c
Bill Ford <wtford@colorado.edu>
2019-01-09 11:08:48 -0700

    LoadClasses check SCRAM_ARCH

--- src/RA2bZinvLoadClasses.C
+++ src/RA2bZinvLoadClasses.C
@@ -1,12 +1,14 @@
 {
   gROOT->Reset();
-  bool forSL6 = false;
-  if (forSL6) {
+  TString arch = gSystem->Getenv("SCRAM_ARCH");
+  if (arch.Contains("slc6")) {
     gSystem->Load("/cvmfs/cms.cern.ch/slc6_amd64_gcc630/external/boost/1.63.0-cms/lib/libboost_program_options.so");
     gSystem->AddIncludePath(" -I/cvmfs/cms.cern.ch/slc6_amd64_gcc630/external/boost/1.63.0-cms/include ");
-  } else {  // sl7
+  } else if (arch.Contains("slc7")) {
     gSystem->Load("/cvmfs/cms.cern.ch/slc7_amd64_gcc820/external/boost/1.67.0/lib/libboost_program_options.so");
     gSystem->AddIncludePath(" -I/cvmfs/cms.cern.ch/slc7_amd64_gcc820/external/boost/1.67.0/include ");
+  } else {
+    cout << "Unknown SCRAM_ARCH = " << arch << "; can't load boost library.";
   }
   gROOT->ProcessLine(".L CCbinning.C+");
   gROOT->ProcessLine(".L RA2bZinvAnalysis.C+");
