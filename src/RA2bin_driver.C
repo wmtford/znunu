64d5028817a37bb9eba40ec940bd6d8f290d96e3
Bill Ford <wtford@colorado.edu>
2019-03-12 15:32:06 -0600

    Update input files

--- src/RA2bin_driver.C
+++ src/RA2bin_driver.C
@@ -5,56 +5,58 @@
   bool do2016 = false;
   bool do2017 = false;
   bool do2018 = false;
-  bool do2018AB = true;
+  bool do2018AB = false;
   bool do2018CD = false;
-  bool doRunw = false;
+  bool doRun2 = false;
+
+  // MC k-factor correction (Run 2):  (1.257*35.9+1.165*(136.8-35.9))/(1.23*136.8)
 
   if (do2016) {
     std::vector< std::pair<TString, float> > gJetsInputs = {
-      std::make_pair("../datFiles/gJets_2016v16_DR2016wt", 35.9)
+      std::make_pair("../datFiles/gJets_2016v16_DRRun2wt", 35.9)
     };
-    RA2bin_inputs_Zinv(Signal, gJetsInputs, "../datFiles/DR_2016v16_DR2016wt_noPU", "../datFiles/DY_2016v16_noPU",
-		       "../plots/histograms/ZinvMCttzMC174bin_2016v16.root", 35.9/35.9, true);
+    RA2bin_inputs_Zinv(Signal, gJetsInputs, "../datFiles/DR_Run2v16_HT17wt_ZptWt_DRRun2wt_noPU", "../datFiles/DY_2016v16_noPU",
+		       "../plots/histograms/ZinvMCttzMC174bin_2016v16_noPU.root", 35.9/35.9, true);
   }
   else if (do2017) {
     std::vector< std::pair<TString, float> > gJetsInputs = {
-      std::make_pair("../datFiles/gJets_2017v16_ZptWt_DR2017wt", 41.5)
+      std::make_pair("../datFiles/gJets_2017v16_HT17wt_ZptWt_DRRun2wt", 41.5)
     };
-    RA2bin_inputs_Zinv(Signal, gJetsInputs, "../datFiles/DR_2017v16_ZptWt_DR2017wt", "../datFiles/DY_2017v16_ZptWt",
-		       "../plots/histograms/ZinvMCttzMC174bin_2017v16_ZptWt.root", 41.5/41.5, true);
+    RA2bin_inputs_Zinv(Signal, gJetsInputs, "../datFiles/DR_Run2v16_HT17wt_ZptWt_DRRun2wt_noPU", "../datFiles/DY_2017v16_ZptWt",
+		       "../plots/histograms/ZinvMCttzMC174bin_2017v16_HT17_ZptWt.root", 41.5/41.5, true);
   }
   else if (do2018AB) {
     std::vector< std::pair<TString, float> > gJetsInputs = {
-      std::make_pair("../datFiles/gJets_2018v16_ZptWt_DR2017wt", 21.1)
+      std::make_pair("../datFiles/gJets_2018v16_HT17wt_ZptWt_DRRun2wt", 21.1)
     };
-    RA2bin_inputs_Zinv(Signal, gJetsInputs, "../datFiles/DR_2018ABv16_ZptWt_DR2017wt", "../datFiles/DY_2018ABv16_ZptWt_noJ",
-		       "../plots/histograms/ZinvMCttzMC174bin_2017v16_ZptWt.root", 21.1/41.5, true);
+    RA2bin_inputs_Zinv(Signal, gJetsInputs, "../datFiles/DR_Run2v16_HT17wt_ZptWt_DRRun2wt_noPU", "../datFiles/DY_2018ABv16_ZptWt_noJ",
+		       "../plots/histograms/ZinvMCttzMC174bin_2018v16_HT17_ZptWt.root", 21.1/59.2, true);
   }
   else if (do2018CD) {
     std::vector< std::pair<TString, float> > gJetsInputs = {
-      std::make_pair("../datFiles/gJets_2018HEMv16_ZptWt_DR2017wt", 38.3)
+      std::make_pair("../datFiles/gJets_2018HEMv16_HT17wt_ZptWt_DRRun2wt", 38.3)
     };
-    RA2bin_inputs_Zinv(Signal, gJetsInputs, "../datFiles/DR_2018CDv16_ZptWt_DR2017wt", "../datFiles/DY_2018CDv16_ZptWt_noJ",
-		       "../plots/histograms/ZinvMCttzMC174bin_2017v16_ZptWt.root", 38.3/41.5, true);
+    RA2bin_inputs_Zinv(Signal, gJetsInputs, "../datFiles/DR_Run2v16_HT17wt_ZptWt_DRRun2wt_noPU", "../datFiles/DY_2018CDv16_ZptWt_noJ",
+		       "../plots/histograms/ZinvMCttzMC174bin_2018HEMv16_HT17_ZptWt.root", 38.3/59.2, true);
   }
   else if (do2018) {
     std::vector< std::pair<TString, float> > gJetsInputs = {
-      std::make_pair("../datFiles/gJets_2018v16_ZptWt_DR2017wt", 21.1),
-      std::make_pair("../datFiles/gJets_2018HEMv16_ZptWt_DR2017wt", 38.3)
+      std::make_pair("../datFiles/gJets_2018v16_HT17wt_ZptWt_DRRun2wt", 21.1),
+      std::make_pair("../datFiles/gJets_2018HEMv16_HT17wt_ZptWt_DRRun2wt", 38.3)
     };
     cout << "No DR, DY file for 2018" << endl;
-    // RA2bin_inputs_Zinv(Signal, gJetsInputs, "../datFiles/DR_Run2v16_ZptWt_DR2017wt_noPU", "../datFiles/DY_Run2v16_ZptWt_noJ",
+    // RA2bin_inputs_Zinv(Signal, gJetsInputs, "../datFiles/DR_Run2v16_HT17wt_ZptWt_DRRun2wt_noPU", "../datFiles/DY_Run2v16_ZptWt_noJ",
     // 		       "../plots/histograms/ZinvMCttzMC174bin_Run2v16_ZptWt.root", 136.8/136.8, true);
   }
   else if (doRun2) {
     std::vector< std::pair<TString, float> > gJetsInputs = {
-      std::make_pair("../datFiles/gJets_2016v16_DR2016wt", 35.9),
-      std::make_pair("../datFiles/gJets_2017v16_ZptWt_DR2017wt", 41.5),
-      std::make_pair("../datFiles/gJets_2018v16_ZptWt_DR2017wt", 21.1),
-      std::make_pair("../datFiles/gJets_2018HEMv16_ZptWt_DR2017wt", 38.3)
+      std::make_pair("../datFiles/gJets_2016v16_DRRun2wt", 35.9),
+      std::make_pair("../datFiles/gJets_2017v16_HT17wt_ZptWt_DRRun2wt", 41.5),
+      std::make_pair("../datFiles/gJets_2018v16_HT17wt_ZptWt_DRRun2wt", 21.1),
+      std::make_pair("../datFiles/gJets_2018HEMv16_HT17wt_ZptWt_DRRun2wt", 38.3)
     };
-    RA2bin_inputs_Zinv(Signal, gJetsInputs, "../datFiles/DR_Run2v16_ZptWt_DR1617wt_noPU", "../datFiles/DY_Run2v16_ZptWt_noJ",
-		       "../plots/histograms/ZinvMCttzMC174bin_Run2v16_ZptWt.root", 136.8/136.8, true);
+    RA2bin_inputs_Zinv(Signal, gJetsInputs, "../datFiles/DR_Run2v16_HT17wt_ZptWt_DRRun2wt_noPU", "../datFiles/DY_Run2v16_ZptWt_noJ",
+		       "../plots/histograms/ZinvMCttzMC174bin_Run2v16_HT17_ZptWt.root", 136.8/136.8, true);
   }
 
 
