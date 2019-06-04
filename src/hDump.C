272698e749277f83b183098854caa07d9b50a676
Bill Ford <wtford@colorado.edu>
2019-06-04 11:18:48 -0600

    Print out errors as well as content

--- src/hDump.C
+++ src/hDump.C
@@ -1,6 +1,12 @@
 void hDump(TH1* histo) {
 
-  for (int i=1; i<= histo->GetNbinsX(); ++i)
-    cout << i << "  " << histo->GetXaxis()->GetBinLabel(i) << "  " << histo->GetBinContent(i) << endl;
+  for (int i=1; i<= histo->GetNbinsX(); ++i) {
+    double y = histo->GetBinContent(i);
+    double yerr = histo->GetBinError(i);
+    double ferr = y > 0 ? yerr/y : 0;
+    char buf[256];
+    sprintf(buf, "%s %11.3f %8.3f (%5.1f\%)", histo->GetXaxis()->GetBinLabel(i), y, yerr, 100*ferr);
+    cout << buf << endl;
+  }
 
 }
