41752de28fceb47e4cd30a3fb75fa47845e3009d
Bill Ford <wtford@colorado.edu>
2019-06-17 16:26:20 -0600

    Fix format bug in hDump

--- src/hDump.C
+++ src/hDump.C
@@ -5,7 +5,7 @@ void hDump(TH1* histo) {
     double yerr = histo->GetBinError(i);
     double ferr = y > 0 ? yerr/y : 0;
     char buf[256];
-    sprintf(buf, "%s %11.3f %8.3f (%5.1f\%)", histo->GetXaxis()->GetBinLabel(i), y, yerr, 100*ferr);
+    sprintf(buf, "%s %11.3f %8.3f (%5.1f%%)", histo->GetXaxis()->GetBinLabel(i), y, yerr, 100*ferr);
     cout << buf << endl;
   }
 
